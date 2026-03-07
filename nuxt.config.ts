927f62659ac57fcf99d7235725efad2ebb391fb0
komkem <komkem.contact@gmail.com>
2026-03-08 00:09:06 +0700

    chore: initialize project with Nuxt 3 and TypeScript configuration
    - Add package.json with dependencies for Nuxt, Vue, and TypeScript
    - Include scripts for building, developing, generating, and previewing the application
    - Create tsconfig.json extending Nuxt's TypeScript configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2026-03-08',
+  devtools: { enabled: true },
+  telemetry: false,
+})
